2ef9ed307f662a3ac7effbbd0c6f0128313de338
kleber-matos <klebermatos2019@gmail.com>
2025-05-16 12:18:49 -0300

    feat: add config imgs file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 const port = 3000;
 
-app.use("/images", express.static("public/images"));
-
 app.use(cors());
 app.use(express.json());
+
+// Serve as imagens da pasta 'public/images' de forma absoluta
+app.use("/images", express.static(path.join(__dirname, "public", "images")));
 
 const produtosRoutes = require("../src/routes/produtosRoutes");
 app.use("/", produtosRoutes);
